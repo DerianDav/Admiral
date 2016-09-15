795b4c8ab75b83ef9ce6a6249bf5d48b1670cbd2
Derian Davila Acuna <1blackbear11@gmail.com>
2016-09-14 20:10:32 -0700

    Version 0.01

---
+++ Assets/ShipTypes.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipType
+{
+    public class standaradShipP
+    {
+        int speed { set; get; }
+        int ConningT { set; get; }
+        int mainGuns { set; get; }
+        int antiAir { set; get; }
+        int secondaryGuns { set; get; }
+
+
+    }
+
+
+
+    public class BB
+    {
+        Ships type = Ships.Battleship;
+        int maxTonnage = 45000;
+        int speed { set; get; }
+        int ConningT { set; get; }
+        int bowGuns { set; get; }
+
+
+    }
+}
---
+++ Assets/Ships.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Ships
+{
+    Auxiliary = 1,
+    Battleship = 2,
+    Carrier = 3,
+    Destroyer = 4,
+    HCruiser = 5,
+    LCruiser = 6,
+    Minesweeper = 7,
+    Submarine = 8
+
+}
